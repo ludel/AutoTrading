f6a7c5c4d5a5e595d55e8481939d8e3c45c89358
ludel <ludovic.delsol@ynov.com>
2021-08-17 13:23:38 +0200

    Model

--- models/custom_gym_env_keras.py
+++ models/custom_gym_env_keras.py
@@ -1,20 +1,21 @@
 import itertools
 
-import pandas as pd
 from keras.layers import Dense, Activation, Flatten
 from keras.models import Sequential
 from keras.optimizers import Adam
 from rl.agents.dqn import DQNAgent
 from rl.memory import SequentialMemory
-from rl.policy import EpsGreedyQPolicy, LinearAnnealedPolicy, MaxBoltzmannQPolicy
+from rl.policy import MaxBoltzmannQPolicy
 
 from env.trading import TradingEnv
+from preprocessing.feature import read_csv
 
 MAX_STEP = 300
 MAX_EPISODE = 2
 
-# data = [read_csv('../data/banque/bnp_1h_2d.csv')]
-data = [pd.read_csv('../data/coinbase-1h-btc-usd.csv').drop(['Date', 'Symbol'], axis=1)]
+train_data = [read_csv('../data/banque/bnp_1h_2d.csv'), read_csv('../data/banque/gle_1h_2d.csv'),
+              read_csv('../data/banque/aca_1h_2d.csv')]
+test_data = read_csv('../data/banque/kn_1h_2d.csv')
 
 metric_choices = ['mse']
 eps_choices = [.1]
@@ -23,11 +24,8 @@ reward_method_choices = ['sortino_ratio']
 lr_choices = [0.001]
 
 
-# enable_dueling_network = [True, False]
-
-# data = list(get_banque_data())
 def create_train(metric, eps, window_size, reward_method, lr):
-    env = TradingEnv(data, initial_account=20_000, window_size=window_size, random_first_step=False,
+    env = TradingEnv(train_data, initial_account=20_000, window_size=window_size, random_first_step=False,
                      reward_method=reward_method)
     nb_actions = env.action_space.n
 
@@ -43,24 +41,24 @@ def create_train(metric, eps, window_size, reward_method, lr):
 
     memory = SequentialMemory(limit=5000, window_length=window_size)
     policy = MaxBoltzmannQPolicy()
-    #policy = LinearAnnealedPolicy(EpsGreedyQPolicy(0.2), attr='eps', value_max=1.0, value_min=0.1, value_test=0,
-    #                              nb_steps=MAX_STEP)
     dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=100, target_model_update=1e-2,
                    policy=policy)
     dqn.compile(Adam(lr), metrics=[metric])
-    dqn.fit(env, nb_steps=15000, visualize=False, verbose=2)
-    env.current_step = 10_000
+
+    dqn.fit(env, nb_steps=50_000, visualize=False, verbose=2)
+    env.current_step = env.window_size
     env.random_first_step = False
+    env.all_df = [test_data]
+    env.reset()
     dqn.test(env, nb_episodes=1, visualize=False)
+
     return env.portfolio.net_worth, dqn, env
-    # env.plot()
 
 
 if __name__ == '__main__':
     best_score = 0
     best_agent = None
     best_env = None
-    print('===> New best score <5866.625579833985> - mae - 0.1 - 10 - sortino_ratio')
     product = [metric_choices, eps_choices, windows_choices, reward_method_choices, lr_choices]
     for combination in itertools.product(*product):
         net_worth, current_agent, current_env = create_train(*combination)
@@ -70,4 +68,4 @@ if __name__ == '__main__':
             best_score = net_worth
             best_agent = current_agent
             best_env = current_env
-            current_env.plot()
+            current_env.plot(plot_marker=False)
